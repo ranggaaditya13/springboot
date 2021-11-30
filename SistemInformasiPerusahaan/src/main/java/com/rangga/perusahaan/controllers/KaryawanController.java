dd35bf2d07dd53617a6548a44163b0d0047374f5
ranggaaditya13 <lenovorangga@gmail.com>
2021-11-30 14:38:46 +0700

    Karyawan Update
    Karyawan Update

--- SistemInformasiPerusahaan/src/main/java/com/rangga/perusahaan/controllers/KaryawanController.java
+++ SistemInformasiPerusahaan/src/main/java/com/rangga/perusahaan/controllers/KaryawanController.java
@@ -3,6 +3,8 @@ package com.rangga.perusahaan.controllers;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,23 +18,27 @@ import com.rangga.perusahaan.services.KaryawanService;
 @RequestMapping("/karyawan")
 public class KaryawanController {
 	
+	private Logger log = LogManager.getLogger(KaryawanController.class.getName());
+	
 	@Autowired
 	private KaryawanService ks;
 	
 	@RequestMapping("/add")
 	public int insertKaryawan(@RequestBody KaryawanModel km) {
+		log.info("Data Berhasil Ditambahkan");
 		return ks.insertKaryawan(km.getNis(), km.getNama(), km.getTempatLahir(),
-				km.getTanggalLahir(), km.getAlamat(), km.getNoHp());
-	
+				km.getTanggalLahir(), km.getAlamat(), km.getNoHp());	
 	}
 	
 	@GetMapping("/data")
 	public List<Map<String, Object>> getAllData(){
+		log.info("Data Berhasil Diambil");
 		return ks.getAllData();
 	}
 	
 	@RequestMapping("/delete")
 	public void deleteData(@RequestBody KaryawanModel km) {
+		log.info("Data Berhasil Dihapus");
 		ks.deleteDataQuery(km.getNis());
 	}
 	
@@ -40,6 +46,9 @@ public class KaryawanController {
 	public void updateData (@RequestBody KaryawanModel km){
 		ks.updateData(km.getNama(),km.getTempatLahir(),km.getTanggalLahir(),
 		km.getAlamat(),km.getNoHp(),km.getNis());
+		log.info("Data Berhasil di ubah");
 		}
+	
+	
 
 }
